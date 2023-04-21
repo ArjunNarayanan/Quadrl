3cb4f5ee6adbe2803812a9aebc280244f52431db
Arjun Narayanan <arjun.narayanan91@gmail.com>
2023-04-21 09:09:35 -0700

    trained model with cleanup at each step

--- examples/demonstrate_operations.jl
+++ examples/demonstrate_operations.jl
@@ -8,8 +8,13 @@ data_filename = "output/model-1/best_model.bson"
 data = BSON.load(data_filename)[:data]
 policy = data["policy"]
 
-wrapper = RandPolyEnv(10, 20, "catmull-clark", false)
+evaluator = BSON.load("output/model-1/evaluator.bson")[:data]["evaluator"]
+fig, ax = plot_normalized_returns(evaluator.mean_returns, evaluator.std_returns)
+ax.set_ylim([0.,1.])
+fig.savefig("output/model-1/returns.png")
 
+
+wrapper = RandPolyEnv(10, 20, "catmull-clark", true)
 # PPO.reset!(wrapper)
 # original_wrapper = deepcopy(wrapper)
 # fig = plot_wrapper(original_wrapper)
@@ -20,7 +25,7 @@ wrapper = RandPolyEnv(10, 20, "catmull-clark", false)
 # fig.savefig("figures/example_meshes/mesh-5-improved.png")
 
 # ret, dev = average_normalized_returns(policy, wrapper, 100)
-# ret, dev = average_normalized_best_returns(policy, wrapper, 100)
+ret, dev = average_normalized_best_returns(policy, wrapper, 100)
 
 
 # mesh, d0 = initialize_random_mesh(10, "catmull-clark")
@@ -29,6 +34,6 @@ wrapper = RandPolyEnv(10, 20, "catmull-clark", false)
 # plot_wrapper(wrapper)
 # ret, dev = average_normalized_returns(policy, wrapper, 100)
 
-# root_dir = "figures/model-1/rollout-3"
+# root_dir = "output/model-1/figures/rollout-2"
 # PPO.reset!(wrapper)
 # plot_trajectory(policy, wrapper, root_dir)
